bab4e54f3bdd2cf84eebc19a65913a887efbd92f
jeroenheijmans <github@jeroenheijmans.nl>
2012-07-08 11:23:56 +0200

    Added a basic "Series" domain object.

---
+++ Bieb.Domain/Entities/Series.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bieb.Domain.Entities
+{
+    class Series : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string SubTitle { get; set; }
+
+        // TODO: This should be an ordered list.
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
