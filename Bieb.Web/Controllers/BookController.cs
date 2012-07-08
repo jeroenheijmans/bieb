135a3999cf340b404e1bfb57fd1b63e96a45ed0a
jeroenheijmans <jeroenheijmans@localhost>
2012-07-08 18:04:45 +0200

    Added book controller for some preliminary testing. Made everything virtual for NHibernate.

---
+++ Bieb.Web/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bieb.Domain.Entities;
+using Bieb.Domain.Repositories;
+
+namespace Bieb.Web.Controllers
+{
+    public class BookController : EntityController<Book>
+    {
+        public BookController(IEntityRepository<Book> Repository) : base(Repository)
+        { }
+    }
+}
