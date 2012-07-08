529aceeb8e9587d66d74c35970589b71df1767aa
jeroenheijmans <github@jeroenheijmans.nl>
2012-07-08 11:21:20 +0200

    First commit. Starting a Hg repo for keeping milestones and having backups. - Default MVC3 project (just the VStudio generated one, unused) - Small start on the domain objects

---
+++ Bieb.Domain/Entities/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bieb.Domain.Entities
+{
+    class Person : BaseEntity
+    {
+        public Alias Name { get; set; }
+
+        public IEnumerable<Alias> Aliases { get; set; }
+        
+        public Gender Gender { get; set; }
+
+        public string Nationality { get; set; }
+
+        public string PlaceOfDeath { get; set; }
+        public string PlaceOfBirth { get; set; }
+
+        // TODO: Implement a "UncertainDate" solution. See also: http://stackoverflow.com/questions/6431908/how-to-design-date-of-birth-in-db-and-orm-for-mix-of-known-and-unkown-dates
+        public string DateOfBirth { get; set; }
+        public string DateOfDeath { get; set; }
+
+        public IEnumerable<Book> EditedBooks { get; set; }
+
+        public IEnumerable<Story> AuthoredStories { get; set; }
+        public IEnumerable<Story> TranslatedStories { get; set; }
+
+        /// <summary>
+        /// All the tags from stories in edited books, plus those from translated- and authored stories
+        /// </summary>
+        public IEnumerable<Tag> Tags
+        {
+            get
+            {
+                return EditedBooks.SelectMany(book => book.Tags)
+                        .Union(
+                            AuthoredStories.SelectMany(story => story.Tags)
+                        )
+                        .Union(
+                            TranslatedStories.SelectMany(story => story.Tags)
+                        );
+            }
+        }
+    }
+    
+    public enum Gender
+    {
+        Male = 'M',
+        Female = 'F',
+        Unkown = '?',
+
+        /// <summary>
+        /// For example when one Author "Person" is in fact many different people
+        /// </summary>
+        None = '-'        
+    }
+}
---
+++ Bieb.Domain/Entities/Story.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bieb.Domain.Entities
+{
+    class Story : BaseEntity
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+
+        public Publisher Publisher { get; set; }
+
+        public IEnumerable<Tag> Tags { get; set; }
+
+        public IEnumerable<Person> Authors { get; set; }
+        public IEnumerable<Person> Translators { get; set; }
+    }
+}
