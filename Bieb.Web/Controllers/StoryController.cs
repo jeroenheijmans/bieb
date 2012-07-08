aef9787f858370ca1b46aaa9dd0276821a816a38
jeroenheijmans <jeroenheijmans@localhost>
2012-07-08 23:16:48 +0200

    Even more Book scaffolding.

---
+++ Bieb.Web/Controllers/StoryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bieb.Domain.Entities;
+using Bieb.Domain.Repositories;
+
+namespace Bieb.Web.Controllers
+{
+    public class StoryController : EntityController<Story>
+    {
+        public StoryController(IEntityRepository<Story> Repository)
+            : base(Repository)
+        { }
+        
+    }
+}
