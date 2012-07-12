fd0c5a6363c87511e8e5ac7a44f34d13ae7b25b5
jeroenheijmans <github@jeroenheijmans.nl>
2012-07-12 20:41:25 +0200

    Integrated style and some layout that will become the main style.

--- Bieb.Domain/Entities/Publisher.cs
+++ Bieb.Domain/Entities/Publisher.cs
@@ -8,7 +8,13 @@ namespace Bieb.Domain.Entities
     public class Publisher : BaseEntity
     {
         public virtual string Name { get; set; }
-        
+
+        private IList<Book> _books = new List<Book>();
+        public virtual IEnumerable<Book> Books
+        {
+            get { return _books; }
+        }
+
         public override string ToString()
         {
             return Name ?? "Publisher (id: " + Id.ToString() + ")";
