0ea998103479a8342b362599ef921d47422b3e37
jeroenheijmans <github@jeroenheijmans.nl>
2012-08-02 09:49:35 +0200

    Basic Search implemented, albeit with some workarounds until we have a proper framework in use (perhaps NHibernate Search?).

---
+++ Bieb.Web/Models/BasicSearchResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bieb.Domain.Entities;
+
+namespace Bieb.Web.Models
+{
+    public class BasicSearchResultModel
+    {
+        public string query;
+        public IEnumerable<Person> people { get; set; }
+        public IEnumerable<Book> books { get; set; }
+        public IEnumerable<Story> stories { get; set; }
+    }
+}
