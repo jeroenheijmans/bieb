4599dd677d3395e48dab97f1466b83cc1efe52e5
jeroenheijmans <github@jeroenheijmans.nl>
2012-08-02 23:14:53 +0200

    Exceptions are expensive and evil, but still created one. Views in the Story controller fall under the Books menu item.

--- Bieb.Tests/WebHelpers/ActionLinkExtensionsTest.cs
+++ Bieb.Tests/WebHelpers/ActionLinkExtensionsTest.cs
@@ -15,6 +15,7 @@ namespace Supr.Tests
         [Test]
         public void MenuLink_Sets_Class_To_Active()
         {
+            // Arrange
             var routeData = new RouteData();
             routeData.Values.Add("controller", "Home");
             routeData.Values.Add("action", "Index");
@@ -28,8 +29,37 @@ namespace Supr.Tests
             string linkText = "clicky, clicky!";
             string actionName = "Index";
             string controllerName = "Home";
+
+            // Act
             MvcHtmlString actual = ActionLinkExtensions.MenuLink(htmlHelper, linkText, actionName, controllerName);
 
+            // Assert
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual.ToHtmlString(), Contains.Substring("class=\"active\""));
+        }
+
+        [Test]
+        public void MenuLink_For_Story_Sets_Book_To_Active()
+        {
+            // Arrange
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Story");
+            routeData.Values.Add("action", "Index");
+
+            HtmlHelper htmlHelper = CreateHtmlHelper(new ViewDataDictionary("Home link"), routeData);
+
+            Assert.That(htmlHelper, Is.Not.Null);
+            Assert.That(htmlHelper.ViewContext, Is.Not.Null);
+            Assert.That(htmlHelper.ViewContext.RouteData, Is.Not.Null);
+
+            string linkText = "clicky, clicky!";
+            string actionName = "Index";
+            string controllerName = "Book";
+
+            // Act
+            MvcHtmlString actual = ActionLinkExtensions.MenuLink(htmlHelper, linkText, actionName, controllerName);
+
+            // Assert
             Assert.That(actual, Is.Not.Null);
             Assert.That(actual.ToHtmlString(), Contains.Substring("class=\"active\""));
         }
