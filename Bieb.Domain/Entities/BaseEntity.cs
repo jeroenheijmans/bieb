83e151844f9c55f51a10f193b71e51206ae6e0fb
jeroenheijmans <github@jeroenheijmans.nl>
2012-08-06 22:30:52 +0200

    Setter for ID on entities should be internal (but usable by the Tests project).

--- Bieb.Domain/Entities/BaseEntity.cs
+++ Bieb.Domain/Entities/BaseEntity.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Bieb.Tests")]
 
 namespace Bieb.Domain.Entities
 {
     public abstract class BaseEntity
     {
-        public virtual int Id { get; set; }
+        public virtual int Id { get; protected internal set; }
         public virtual int Version { get; set; }
 
         public override string ToString()
