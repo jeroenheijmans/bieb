5aab665624ce3d16ca6598f014daf072dcdeec21
jeroenheijmans <github@jeroenheijmans.nl>
2012-12-21 23:54:04 +0100

    Wired basic logging. Replaced NLog with log4net, given that NHibernate depends on that library anyways. Currently only logging app start/end and controller actions.
    TODO
    * Exception logging incl unhandled exceptions
    * Include SQL for log table used in Ado appender
    * Create a controller + some views for viewing the log in the site (though this
    requires authentication too, so may leave that for later)

---
+++ Bieb.Framework/Logging/Log4netLogger.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Bieb.Framework.Logging
+{
+    /// <summary>
+    /// ILogger implementation using the log4net package. Configuration of log4net is placed in the AssemblyInfo file.
+    /// </summary>
+    public class Log4netLogger : ILogger
+    {
+        private ILog logger;
+
+        public Log4netLogger()
+        {
+            logger = LogManager.GetLogger(this.GetType());
+        }
+
+        public void Log(Level level, string message)
+        {
+            Log(level, message, null);
+        }
+
+        public void Log(Level level, string message, Exception exception)
+        {
+            switch (level)
+            {
+                case Level.Debug:
+                    LogDebug(message);
+                    break;
+                case Level.Information:
+                    LogInformation(message);
+                    break;
+                case Level.Warning:
+                    LogWarning(message);
+                    break;
+                case Level.Error:
+                    LogError(message, exception);
+                    break;
+                case Level.Fatal:
+                    LogFatal(message, exception);
+                    break;
+                default:
+                    LogError("Logging called with unkown level '" + level.ToString() + "'. Message will be logged with substitute level 'information'.");
+                    LogInformation(message);
+                    break;
+            }
+        }
+
+        public void Log(Level level, Exception exception)
+        {
+            Debug.Assert(exception != null, "Argument 'exception' should not be null: logging nothing is quite pointless.");
+            Log(level, "Logging called", exception);
+        }
+        
+        public void LogInformation(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            logger.Warn(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            LogError(message, null);
+        }
+
+        public void LogError(string message, Exception exception)
+        {
+            logger.Error(message, exception);
+        }
+
+        public void LogError(Exception exception)
+        {
+            LogError("An error occurred.", exception);
+        }
+
+        public void LogFatal(string message)
+        {
+            logger.Fatal(message);
+        }
+
+        public void LogFatal(string message, Exception exception)
+        {
+            logger.Fatal(message, exception);
+        }
+
+        public void LogFatal(Exception exception)
+        {
+            LogFatal("A fatal error occurred.", exception);
+        }
+    }
+}
---
+++ Bieb.Web/Infrastructure/Filters/LogFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bieb.Framework.Logging;
+
+namespace Bieb.Web.Infrastructure.Filters
+{
+    public class LogFilter : IActionFilter
+    {
+        private readonly ILogger logger;
+        private readonly Level logLevel;
+
+        public LogFilter(ILogger log, Level logLevel)
+        {
+            this.logger = log;
+            this.logLevel = logLevel;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var message = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "Executing action {0}.{1}",
+                    filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+                    filterContext.ActionDescriptor.ActionName);
+            this.logger.Log(this.logLevel, message);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var message = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "Executed action {0}.{1}",
+                    filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+                    filterContext.ActionDescriptor.ActionName);
+            this.logger.Log(this.logLevel, message);
+        }
+    }
+}
