e49f3163894aae5f2ec01024838dab9d0dbf49b2
jeroenheijmans <github@jeroenheijmans.nl>
2012-12-26 22:01:49 +0100

    Removing test for now - this will be work for later

--- Bieb.Tests/WebHelpers/ImageExtensionsTests.cs
+++ Bieb.Tests/WebHelpers/ImageExtensionsTests.cs
@@ -13,15 +13,6 @@ namespace Bieb.Tests.WebHelpers
     [TestFixture]
     public class ImageExtensionsTests : HtmlHelperTests
     {
-        [Ignore("Need to mock/decouple the VirtualPathUtility to make this test work.")]
-        [Test]
-        public void Can_Create_Flag_Image_With_Dutch_Png()
-        {
-            HtmlHelper htmlHelper = CreateHtmlHelper(new ViewDataDictionary("Home link"), new RouteData());
-
-            var result = htmlHelper.ImageFlagFor(new Person() { Nationality = "nl"});
-
-            Assert.That(result, Is.Not.Null);
-        }
+        // TODO: Need to mock/decouple the VirtualPathUtility to make these tests work
     }
 }
