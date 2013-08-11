180b2af320735e3e400813639517869e713a1100
jeroenheijmans <jeroenheijmans@localhost>
2013-08-11 23:36:32 +0200

    Extending controllers with 404 capabilities + some tests.

--- Bieb.Web/Controllers/LibraryBooksController.cs
+++ Bieb.Web/Controllers/LibraryBooksController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Linq;
 using Bieb.Domain.Entities;
@@ -11,6 +12,10 @@ namespace Bieb.Web.Controllers
     {
         public LibraryBooksController(IEntityRepository<LibraryBook> repository)
             : base(repository)
+        { }
+
+        public LibraryBooksController(IEntityRepository<LibraryBook> repository, HttpResponseBase customResponse, HttpContextBase customContext)
+            : base(repository, customResponse, customContext)
         { }
 
         protected override System.Linq.Expressions.Expression<Func<LibraryBook, IComparable>> SortFunc
