8b224e97b99241b18498a56a33f1657ffccc5a6f
jeroenheijmans <jeroenheijmans@localhost>
2014-03-09 15:58:46 +0100

    Fixed menu "active"ness for Story routes (should make "Books" active).

--- Bieb.Tests/WebHelpers/ActionLinkExtensionsTests.cs
+++ Bieb.Tests/WebHelpers/ActionLinkExtensionsTests.cs
@@ -12,62 +12,32 @@ namespace Bieb.Tests.WebHelpers
     public class ActionLinkExtensionsTest : HtmlHelperTests
     {
         [Test]
-        public void MenuLink_Sets_Class_To_Active()
-        {
-            var routeData = new RouteData();
-            routeData.Values.Add("controller", "Home");
-            routeData.Values.Add("action", "Index");
-
-            HtmlHelper htmlHelper = CreateHtmlHelper(new ViewDataDictionary("Home link"), routeData);
-
-            const string linkText = "clicky, clicky!";
-            const string actionName = "Index";
-            const string controllerName = "Home";
-
-            MvcHtmlString actual = htmlHelper.MenuLink(linkText, actionName, controllerName);
-
-            Assert.That(actual, Is.Not.Null);
-            Assert.That(actual.ToHtmlString(), Contains.Substring("class=\"active\""));
-        }
-
-
-        [Test]
-        public void MenuLink_For_Story_Sets_Book_To_Active()
+        public void Can_Generate_Active_CssClass_For_Book_On_Story_Controller()
         {
             var routeData = new RouteData();
             routeData.Values.Add("controller", "Story");
             routeData.Values.Add("action", "Index");
 
-            HtmlHelper htmlHelper = CreateHtmlHelper(new ViewDataDictionary("Home link"), routeData);
-            
-            const string linkText = "clicky, clicky!";
-            const string actionName = "Index";
-            const string controllerName = "Books";
+            HtmlHelper htmlHelper = CreateHtmlHelper(new ViewDataDictionary("Story link"), routeData);
 
-            MvcHtmlString actual = htmlHelper.MenuLink(linkText, actionName, controllerName);
+            var cssClass = htmlHelper.GetCssClass("Books");
 
-            Assert.That(actual, Is.Not.Null);
-            Assert.That(actual.ToHtmlString(), Contains.Substring("class=\"active\""));
+            Assert.That(cssClass, Is.EqualTo("active"));
         }
 
 
         [Test]
-        public void MenuLink_For_Stories_Sets_Book_To_Active()
+        public void Can_Generate_Active_CssClass_For_Book_On_Stories_Controller()
         {
             var routeData = new RouteData();
             routeData.Values.Add("controller", "Stories");
             routeData.Values.Add("action", "Index");
 
-            HtmlHelper htmlHelper = CreateHtmlHelper(new ViewDataDictionary("Home link"), routeData);
+            HtmlHelper htmlHelper = CreateHtmlHelper(new ViewDataDictionary("Story link"), routeData);
             
-            const string linkText = "clicky, clicky!";
-            const string actionName = "Index";
-            const string controllerName = "Books";
+            var cssClass = htmlHelper.GetCssClass("Books");
 
-            MvcHtmlString actual = htmlHelper.MenuLink(linkText, actionName, controllerName);
-
-            Assert.That(actual, Is.Not.Null);
-            Assert.That(actual.ToHtmlString(), Contains.Substring("class=\"active\""));
+            Assert.That(cssClass, Is.EqualTo("active"));
         }
 
 
--- Bieb.Web/Helpers/ActionLinkExtensions.cs
+++ Bieb.Web/Helpers/ActionLinkExtensions.cs
@@ -5,28 +5,13 @@ namespace Bieb.Web.Helpers
 {
     public static class ActionLinkExtensions
     {
-        public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName)
-        {
-            string currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
-
-            object htmlAttributes = null;
-
-            if (controllerName == currentController 
-                // TODO: Refactor the following two "Exceptions" to a more solid solution.
-                || (controllerName == "Books" && currentController == "Story")
-                || (controllerName == "Books" && currentController == "Stories"))
-            {
-                htmlAttributes = new { @class = "active" };
-            }
-
-            return htmlHelper.ActionLink(linkText, actionName, controllerName, null, htmlAttributes);
-        }
-
-
         public static string GetCssClass(this HtmlHelper htmlHelper, string controllerName)
         {
             string currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
-            if (controllerName == currentController)
+            if (controllerName == currentController
+                // TODO: Refactor the following two "Exceptions" to a more solid solution.
+                || (controllerName == "Books" && currentController == "Story")
+                || (controllerName == "Books" && currentController == "Stories"))
             {
                 return "active";
             }
