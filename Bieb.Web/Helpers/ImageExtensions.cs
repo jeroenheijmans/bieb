39036d6518d95e46d8802b2b41f01830ebff35d4
jeroenheijmans <github@jeroenheijmans.nl>
2014-06-09 12:02:08 +0200

    ImageExtensions are now under test, after fighting MVCs nontestable parts a bit.

--- Bieb.Web/Helpers/ImageExtensions.cs
+++ Bieb.Web/Helpers/ImageExtensions.cs
@@ -1,20 +1,21 @@
 ﻿using System.Web;
 using System.Web.Mvc;
-using Bieb.Domain.Entities;
 
 namespace Bieb.Web.Helpers
 {
     public static class ImageExtensions
     {
-        // TODO: Need to wrap VirtualPathUtility in an interface/class to be able to mock it?
-
-        public static IHtmlString ImageFlagFor(this HtmlHelper htmlHelper, string Nationality)
+        public static IHtmlString ImageFlagFor(this HtmlHelper htmlHelper, string nationality)
         {
             var tagBuilder = new TagBuilder("img");
-            tagBuilder.Attributes.Add("alt", Nationality);
-            tagBuilder.Attributes.Add("title", Nationality);
-            tagBuilder.Attributes.Add("src", VirtualPathUtility.ToAbsolute("~/Content/images/flags/" + Nationality + "-icon.png"));
+            
+            tagBuilder.Attributes.Add("alt", nationality);
+            tagBuilder.Attributes.Add("title", nationality);
             tagBuilder.Attributes.Add("class", "flag icon");
+
+            var src = UrlHelper.GenerateContentUrl("~/Content/images/flags/" + nationality + "-icon.png", htmlHelper.ViewContext.HttpContext);
+            tagBuilder.Attributes.Add("src", src);
+
             return new HtmlString(tagBuilder.ToString(TagRenderMode.SelfClosing));
         }
     }
