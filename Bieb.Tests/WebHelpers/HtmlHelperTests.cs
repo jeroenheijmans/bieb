b1945ffddd7300b5f06fb07822d69aee2f8c0644
jeroenheijmans <jeroenheijmans@localhost>
2014-06-09 12:02:08 +0200

    ImageExtensions are now under test, after fighting MVCs nontestable parts a bit.

--- Bieb.Tests/WebHelpers/HtmlHelperTests.cs
+++ Bieb.Tests/WebHelpers/HtmlHelperTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Web.Mvc;
 using NUnit.Framework;
 using System.Web.Routing;
@@ -12,6 +11,7 @@ using System.IO;
 namespace Bieb.Tests.WebHelpers
 {
     [TestFixture]
+    // TODO: An abstract class with only one static method? Refactor that.
     public abstract class HtmlHelperTests
     {
         protected static HtmlHelper<string> CreateHtmlHelper(ViewDataDictionary viewData, RouteData routeData)
@@ -21,6 +21,12 @@ namespace Bieb.Tests.WebHelpers
                 routeData,
                 new Mock<ControllerBase>().Object);
 
+            var httpContext = new Mock<HttpContextBase>();
+            var request = new Mock<HttpRequestBase>();
+
+            httpContext.Setup(h => h.Request).Returns(request.Object);
+            request.Setup(r => r.ApplicationPath).Returns("/");
+            
             var mockViewContext = new Mock<ViewContext>(
                 controllerContext.Object,
                 new Mock<IView>().Object,
@@ -31,13 +37,14 @@ namespace Bieb.Tests.WebHelpers
             var mockViewDataContainer = new Mock<IViewDataContainer>();
 
             mockViewContext.Setup(v => v.RouteData).Returns(routeData);
+            mockViewContext.Setup(v => v.HttpContext).Returns(httpContext.Object);
             mockViewDataContainer.Setup(v => v.ViewData).Returns(viewData);
 
             var htmlHelper = new HtmlHelper<string>(mockViewContext.Object, mockViewDataContainer.Object);
 
-            Assert.That(htmlHelper, Is.Not.Null);
-            Assert.That(htmlHelper.ViewContext, Is.Not.Null);
-            Assert.That(htmlHelper.ViewContext.RouteData, Is.Not.Null);
+            Assert.That(htmlHelper.ViewContext, Is.Not.Null, "CreateHtmlHelper didn't set ViewContext properly.");
+            Assert.That(htmlHelper.ViewContext.RouteData, Is.Not.Null, "CreateHtmlHelper didn't set ViewContext.RouteData properly.");
+            Assert.That(htmlHelper.ViewContext.HttpContext, Is.Not.Null, "CreateHtmlHelper didn't set ViewContext.HttpContext properly.");
 
             return htmlHelper;
         }
---
+++ Bieb.Tests/WebHelpers/ImageExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Bieb.Web.Helpers;
+using NUnit.Framework;
+
+namespace Bieb.Tests.WebHelpers
+{
+    [TestFixture]
+    public class ImageExtensionsTests : HtmlHelperTests
+    {
+        private HtmlHelper htmlHelper;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Story");
+            routeData.Values.Add("action", "Index");
+
+            htmlHelper = CreateHtmlHelper(new ViewDataDictionary("Story link"), routeData);
+        }
+
+
+        [Test]
+        public void ImageFlagFor_Will_Return_Img_Tag()
+        {
+            var result = htmlHelper.ImageFlagFor("NL").ToString();
+            Assert.That(result.StartsWith("<img "));
+            Assert.That(result.EndsWith("/>"));
+        }
+
+
+        [Test]
+        public void ImageFlagFor_Will_Set_Alt_Attribute()
+        {
+            var result = htmlHelper.ImageFlagFor("NL").ToString();
+            StringAssert.IsMatch("alt=['\"].+['\"]", result);
+        }
+
+
+        [Test]
+        public void ImageFlagFor_Will_Set_Title_Attribute()
+        {
+            var result = htmlHelper.ImageFlagFor("NL").ToString();
+            StringAssert.IsMatch("title=['\"].+['\"]", result);
+        }
+
+
+        [Test]
+        public void ImageFlagFor_Will_Set_Class_Attribute()
+        {
+            var result = htmlHelper.ImageFlagFor("NL").ToString();
+            StringAssert.Contains("class=\"flag icon\"", result);
+        }
+
+        
+        [Test]
+        public void ImageFlagFor_Will_Set_Src_Attribute_To_Some_Png()
+        {
+            var result = htmlHelper.ImageFlagFor("NL").ToString();
+            StringAssert.IsMatch("src=['\"].+\\.png['\"]", result);
+        }
+    }
+}
