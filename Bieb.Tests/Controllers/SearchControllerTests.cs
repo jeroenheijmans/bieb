f52d9cb0b0b90396697a7db06e16342e07c492fe
jeroenheijmans <jeroenheijmans@localhost>
2015-02-15 16:43:06 +0100

    Fixing broken tests.

--- Bieb.Tests/Controllers/SearchControllerTests.cs
+++ Bieb.Tests/Controllers/SearchControllerTests.cs
@@ -47,16 +47,16 @@ namespace Bieb.Tests.Controllers
             var searchResults = (BasicSearchResultModel)result.Model;
 
             Assert.That(searchResults.Books.Count(), Is.EqualTo(2));
-            Assert.That(searchResults.Books, Contains.Item(markTheMartian));
-            Assert.That(searchResults.Books, Contains.Item(markmanship));
+            Assert.That(searchResults.Books.Count(b => b.Text == markTheMartian.Title), Is.EqualTo(1), "Can find markTheMartian");
+            Assert.That(searchResults.Books.Count(b => b.Text == markmanship.Title), Is.EqualTo(1), "Can find markTheMartian");
         }
 
 
         [Test]
         public void Can_Find_Person_With_Basic_Search()
         {
-            var aldiss = new Person { FirstName = "Brian", Surname = "Aldiss" };
-            var asimov = new Person { FirstName = "Isaac", Surname = "Asimov" };
+            var aldiss = new Person { Id = 1, FirstName = "Brian", Surname = "Aldiss" };
+            var asimov = new Person { Id = 2, FirstName = "Isaac", Surname = "Asimov" };
 
             peopleRepository.Add(aldiss);
             peopleRepository.Add(asimov);
@@ -66,8 +66,8 @@ namespace Bieb.Tests.Controllers
             var searchResults = (BasicSearchResultModel)result.Model;
 
             Assert.That(searchResults.People.Count(), Is.EqualTo(2));
-            Assert.That(searchResults.People, Contains.Item(aldiss));
-            Assert.That(searchResults.People, Contains.Item(asimov));
+            Assert.That(searchResults.People.Count(p => p.Id == aldiss.Id), Is.EqualTo(1), "Can find Aldiss");
+            Assert.That(searchResults.People.Count(p => p.Id == asimov.Id), Is.EqualTo(1), "Can find Asimov");
         }
 
 
@@ -75,23 +75,22 @@ namespace Bieb.Tests.Controllers
         public void Can_Find_Story_With_Basic_Search()
         {
             var bundle = new Book();
-            var story1 = new Story { Title = "The first story ever", Book = bundle };
-            var story2 = new Story { Title = "The very second story", Book = bundle };
-            var story3 = new Story { Title = "The third ever story", Book = bundle };
+            var story1 = new Story { Id = 1, Title = "The first story ever", Book = bundle };
+            var story2 = new Story { Id = 2, Title = "The very second story", Book = bundle };
+            var story3 = new Story { Id = 3, Title = "The third ever story", Book = bundle };
             bundle.AddStory(0, story1);
             bundle.AddStory(1, story2);
 
             storyRepository.Add(story1);
             storyRepository.Add(story2);
             storyRepository.Add(story3);
-
-
+            
             var result = (ViewResult)controller.Basic("ever");
             var searchResults = (BasicSearchResultModel)result.Model;
 
             Assert.That(searchResults.Stories.Count(), Is.EqualTo(2));
-            Assert.That(searchResults.Stories, Contains.Item(story1));
-            Assert.That(searchResults.Stories, Contains.Item(story3));
+            Assert.That(searchResults.Stories.Count(s => s.Id == story1.Id), Is.EqualTo(1), "Found story 1");
+            Assert.That(searchResults.Stories.Count(s => s.Id == story3.Id), Is.EqualTo(1), "Found story 3");
         }
 
 
