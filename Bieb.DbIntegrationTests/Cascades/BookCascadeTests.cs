b602fbca914da6c05d0cffca0281d5c487b257f7
jeroenheijmans <jeroenheijmans@localhost>
2015-02-23 22:34:22 +0100

    Deleting a book will now also cascade to stories. Deleting a seperate story also possible.

--- Bieb.DbIntegrationTests/Cascades/BookCascadeTests.cs
+++ Bieb.DbIntegrationTests/Cascades/BookCascadeTests.cs
@@ -20,5 +20,19 @@ namespace Bieb.DbIntegrationTests.Cascades
             
             Assert.DoesNotThrow(Session.Flush);
         }
+
+        [Test]
+        public void Delete_Book_Will_Cascade_To_Stories()
+        {
+            var book = new Book("Jupiter vs Pluto");
+            var story = new Story();
+            book.AddStory(story);
+
+            Session.Save(book);
+            Session.Flush();
+            Session.Delete(book);
+
+            Assert.DoesNotThrow(Session.Flush);
+        }
     }
 }
